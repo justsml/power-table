9c08a1b46e76422cf32d1b5bea99160785a09cdf
Dan Levy <dan@danlevy.net>
2016-06-08 22:47:00 -0600

    Sortable update

--- src/plugins/sortable.js
+++ src/plugins/sortable.js
@@ -31,8 +31,10 @@ export function Sortable({table}) {
     const el          = table.querySelector(`th[sort=${sortBy.replace(/-/, '')}]`)
     removeAll(sortIcons)
     if (el) {
-
-
+      let sort = el.getAttribute('sort')
+      let desc = sort.indexOf('-') === 0
+      let icon = createElem(`<b class='sort-arrow'>${desc ? downArrow : upArrow}</b>`)
+      el.appendChild(icon)
     }
   }
 
@@ -62,86 +64,5 @@ export function Sortable({table}) {
     elem.setAttribute('sort', column.sort);
     return arguments[0];
   }
-
-  function selectAll() {
-    Array.from(table.querySelectorAll('.selection-col [type="checkbox"]'))
-      .map(function(el) {return el.value;})
-      .map(selectItem.bind(null, true));
-  }
-
-  function selectNone() {
-    Array.from(table.querySelectorAll('.selection-col [type="checkbox"]'))
-      .map(function(el) {return el.value;})
-      .map(selectItem.bind(null, false));
-  }
-
-  function selectItem(id, bool) {
-    if (typeof bool === 'string' && typeof id === 'boolean') {
-      // reverse params
-      [id, bool] = [bool, id];
-    }
-    if (!id) {return false;}
-
-    var chk = table.querySelector('[type="checkbox"][value="' + id + '"]');
-    if (chk) {
-      // see if we are in 'toggle mode'
-      if (typeof bool === 'undefined' || bool === null) {
-        bool = !chk.checked; // Toggle it!
-      }
-      if (bool) {
-        chk.checked = 'checked';
-        chk.setAttribute('checked', 'checked');
-        chk.parentNode.parentNode.classList.add('selected');
-        if (selected.indexOf(id) === -1) {selected.push(id);}
-      } else {
-        chk.checked = undefined;
-        chk.removeAttribute('checked');
-        chk.parentNode.parentNode.classList.remove('selected');
-        if (selected.indexOf(id) !== -1) {selected.splice(selected.indexOf(id), 1);}
-      }
-    }
-
-    this.setStatusTotals(this.users.length, selected.length);
-
-    return {'id': id, 'checked': bool, 'elem': chk};
-  }
-
-  function getSelected() { return selected; }
-
-  function selectToggle(id) {
-    return selectItem.bind(this)(id, undefined);
-  }
-
-  function selectAdd(id) {
-    return selectItem.bind(this)(id, true);
-  }
-
-  function selectRemove(id) {
-    return selectItem.bind(this)(id, false);
-  }
-
-  function isSelected(id) {
-    return selected.indexOf(id) > -1;
-  }
-
-  function _handleSelect(e) {
-    var el, val;
-    if (e.target.tagName === 'INPUT') {
-      val = e.target.value;
-    } else if (e.target.tagName === 'TR') {
-      el = e.target.querySelector('input[type="checkbox"]');
-      if (el && el.value) { val = el.value; }
-    } else if (e.target.tagName === 'TD') {
-      el = e.target.parentNode.querySelector('input[type="checkbox"]');
-      if (el && el.value) { val = el.value; }
-    }
-
-    console.warn('_handleSelect Triggered', val, el, e);
-    if (val) {
-      e.preventDefault();
-      this.selectToggle(val);
-    }
-  }
-
 
 }
